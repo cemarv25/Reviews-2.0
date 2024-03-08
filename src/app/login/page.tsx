bd5bb77b3c01359e30145e0e2366ca38820a5abf
cemarv25 <cesar.martinez0010@outlook.com>
2024-03-08 00:36:53 -0600

    chore(auth): fix middleware not running and protect routes via next middleware (issue-3)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,7 @@
 'use server';
 import LoginForm from '@/components/forms/login';
-import { createClient } from '@/utils/supabase/server';
-import { redirect } from 'next/navigation';
-
-const supabase = createClient();
 
 const Login = async () => {
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (user) {
-    redirect('/');
-  }
-
   return (
     <div className="p-10 lg:px-32 lg:py-10">
       <LoginForm />
